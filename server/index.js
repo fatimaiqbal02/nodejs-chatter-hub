e09406697299c65a69d43873ea8657d2e3fb072f
Fatima Iqbal <fatimaiqbalmirza002@gmail.com>
2024-07-05 12:42:17 +0500

    Modify index.js

--- server/index.js
+++ server/index.js
@@ -44,4 +44,7 @@ io.on('connection', (socket) => {
     });
 });
 
-module.exports = server;
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
