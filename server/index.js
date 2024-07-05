5e108822fd175c8e7f4d0e69d34be795cce8362b
Fatima Iqbal <fatimaiqbalmirza002@gmail.com>
2024-07-05 13:00:04 +0500

    Modify cors setting

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors());
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://nodejs-chatter-hub.vercel.app');
+    res.setHeader('Access-Control-Allow-Origin', 'https://nodejs-chatter-hub-server.vercel.app');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
