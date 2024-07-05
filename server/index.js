866a87912f9fea977564d1f7a5a6111168aead0e
Fatima Iqbal <fatimaiqbalmirza002@gmail.com>
2024-07-05 13:12:52 +0500

    Modify index.js

--- server/index.js
+++ server/index.js
@@ -17,17 +17,14 @@ app.use((req, res, next) => {
 });
 
 const server = http.createServer(app);
-const io = require("socket.io")(server, {
-    handlePreflightRequest: (req, res) => {
-        const headers = {
-            "Access-Control-Allow-Headers": "Content-Type, Authorization",
-            "Access-Control-Allow-Origin": req.headers.origin, //or the specific origin you want to give access to,
-            "Access-Control-Allow-Credentials": true
-        };
-        res.writeHead(200, headers);
-        res.end();
+const io = require('socket.io')(server, { 
+    cors: {
+        origin: "https://nodejs-chatter-hub.vercel.app",
+        methods: ["GET", "POST"]
     }
 });
+
+
 
 
 const users = {};
